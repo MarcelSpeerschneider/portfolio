003a8f448d60253870b28a755ac149feaabc648a
Marcel Speerschneider <123905572+MarcelSpeerschneider@users.noreply.github.com>
2025-03-02 12:58:10 +0100

    my skills added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,11 +4,12 @@ import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './shared/header/header.component';
 import { LandingPageComponent } from './landing-page/landing-page.component';
 import { AboutMeComponent } from "./about-me/about-me.component";
+import { MySkillsComponent } from "./my-skills/my-skills.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, HeaderComponent, LandingPageComponent, AboutMeComponent],
+  imports: [CommonModule, RouterOutlet, HeaderComponent, LandingPageComponent, AboutMeComponent, MySkillsComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
