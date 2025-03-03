2f3924277618d95715127ae3195a3d0d1e85f934
Marcel Speerschneider <123905572+MarcelSpeerschneider@users.noreply.github.com>
2025-03-03 23:03:24 +0100

    portfolio component started

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,11 +5,12 @@ import { HeaderComponent } from './shared/header/header.component';
 import { LandingPageComponent } from './landing-page/landing-page.component';
 import { AboutMeComponent } from "./about-me/about-me.component";
 import { MySkillsComponent } from "./my-skills/my-skills.component";
+import { PortfolioComponent } from "./portfolio/portfolio.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, HeaderComponent, LandingPageComponent, AboutMeComponent, MySkillsComponent],
+  imports: [CommonModule, RouterOutlet, HeaderComponent, LandingPageComponent, AboutMeComponent, MySkillsComponent, PortfolioComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
