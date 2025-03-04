7e63a5f3dd81d4ee4f3f329f34d47b66f8910334
Marcel Speerschneider <123905572+MarcelSpeerschneider@users.noreply.github.com>
2025-03-05 00:14:32 +0100

    pokedex  added

--- src/app/portfolio/portfolio.component.ts
+++ src/app/portfolio/portfolio.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { JoinComponent } from "./join/join.component";
+import { PokedexComponent } from "./pokedex/pokedex.component";
 
 @Component({
   selector: 'app-portfolio',
   standalone: true,
-  imports: [JoinComponent],
+  imports: [JoinComponent, PokedexComponent],
   templateUrl: './portfolio.component.html',
   styleUrl: './portfolio.component.scss'
 })
