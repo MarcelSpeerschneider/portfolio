8491296daa0d03ae82a3edd4de05e571627d0e4f
Marcel Speerschneider <123905572+MarcelSpeerschneider@users.noreply.github.com>
2025-03-04 23:56:23 +0100

    portfolio section added

--- src/app/portfolio/portfolio.component.ts
+++ src/app/portfolio/portfolio.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { JoinComponent } from "./join/join.component";
 
 @Component({
   selector: 'app-portfolio',
   standalone: true,
-  imports: [],
+  imports: [JoinComponent],
   templateUrl: './portfolio.component.html',
   styleUrl: './portfolio.component.scss'
 })
