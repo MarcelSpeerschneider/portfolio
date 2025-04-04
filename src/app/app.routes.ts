003641232368da709b92ccb1417bcdb98e1f51ea
Marcel Speerschneider <123905572+MarcelSpeerschneider@users.noreply.github.com>
2025-04-05 00:50:51 +0200

    responsiveness added

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,18 +1,13 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
-import { LinkShortenerComponent } from './link-shortener/link-shortener.component';
 import { routeDebugGuard } from './guards/route-debug.guard';
+import { Component } from '@angular/core';
 
 export const routes: Routes = [
     {
         path: '',
         pathMatch: 'full',
         component: HomeComponent,
-        canActivate: [routeDebugGuard]
-    },
-    {
-        path: 'linktest',
-        component: LinkShortenerComponent,
         canActivate: [routeDebugGuard]
     },
     {
